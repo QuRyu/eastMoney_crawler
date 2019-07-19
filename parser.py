7f8c3920d92943b8573c2b66377b4a400acf324c
QuRyu <Quentin.Liu.0415@gmail.com>
2019-07-19 09:22:59 +0800

    Refactored Url class into functions

--- parser.py
+++ parser.py
@@ -6,6 +6,8 @@ from multiprocessing import Pool
 import requests 
 import sqlite3 
 
+def generate_url(page_number: int) -> str:
+    return "http://data.eastmoney.com/DataCenter_V3/jgdy/xx.ashx?pagesize=50&page={}&js=var%20AoofQLPM&param=&sortRule=-1&sortType=0&rt=52045947".format(page_number)
 
 # check if the table with the name has been created 
 def checkTableExistence(conn, name): 
@@ -121,65 +123,37 @@ class Indexer:
     def total_range(self) -> (int, int): 
         return (1, (self.total_page-1)*50 + self.last_page_items)
 
-
-class UrlGenerator: 
-    def __init__(self, conn):
-        self._conn = conn 
-        latest_page = self._get_latest_page() 
-        total_pages = self._total_page_number()
-
-        self._idx = 1
-        if latest_page == None: # read in all pages 
-            self._end = total_pages
-        else: # only download updated part 
-            diff = total_pages-latest_page
-            self._end = 1 if diff == 0 else diff 
-
-    # yield the next url for pulling data 
-    # return value None indicates that the list of urls to pull data from is exhausted 
-    def yield_url(self):
-        if self._idx <= self._end: 
-            url = "http://data.eastmoney.com/DataCenter_V3/jgdy/xx.ashx?pagesize=50&page={}&js=var%20AoofQLPM&param=&sortRule=-1&sortType=0&rt=52045947".format(self._idx)
-            print("parsing page ", self._idx)
-            self._idx += 1 
-            return url 
-        else: 
-            return None 
-
-    def update_db(self):
-        date = datetime.datetime.now()
-        cursor = self._conn.cursor()
-        cursor.execute("INSERT INTO history VALUES (?,?)", [str(date.date()), self._total_pages])
-        self._conn.commit()
- 
-    def _get_pull_history(self):
-        if not checkTableExistence(self._conn, "pull"):
-            self._create_table()
-            return None 
-        
-        # check if there is any query before 
-        cursor = self._conn.cursor()
-        cursor.execute("SELECT * FROM history ORDER BY index_start")
-        history = cursor.fetchall()
-
-        if len(history) == 0:
-          return None
-        else:
-            # merge consecutive ranges into larger ones
-            history = merge_ranges(history)  
-
-    def _total_page_number(self): 
-        URL = "http://data.eastmoney.com/DataCenter_V3/jgdy/xx.ashx?pagesize=50&page=1&js=var%20AoofQLPM&param=&sortRule=-1&sortType=0&rt=52045947"
-        data = getPageContent(URL)
-        self._total_pages = data['pages']
-
-        return self._total_pages
-
-    def _create_table(self):
-        c = self._conn.cursor() 
+def pull_history(conn, indexer: Indexer) -> [(int, int)]:
+    c = conn.cursor()
+    if not checkTableExistence("history"): # create the table 
         c.execute('''CREATE TABLE history
                      (index_start INTEGER, index_end INTEGER)''')
-        self._conn.commit()
+        conn.commit()
+        return None 
+    else:
+        c.execute("SELECT * FROM history ORDER BY index_start")
+        history = cursor.fetch_all()
+        if len(history) == None:
+            return None
+        else:
+            ranges = merge_ranges(history)
+            return ranges
+
+def update_history_db(cursor, start, end): 
+    cursor.execute("INSERT INTO history VALUES (?,?)", [start, end])
+
+# returns (number of pages, #items in last page) 
+def get_page_info() -> (int, int):
+    def total_page_number(): 
+        URL = generate_url(1)
+        data = getPageContent(URL)
+        return data['pages']
+
+    N = total_page_number()
+    URL = generate_url(N)
+    data = getPageContent(URL)['data']
+
+    return (N, len(data))
 
 
 class Scheduler:
